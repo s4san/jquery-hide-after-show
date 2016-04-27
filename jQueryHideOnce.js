6802ceea9de2544801e17a4e4f892b4fa55568fb
s4san <sandeepkt18@yahoo.com>
2016-04-27 21:55:13 +0530

    Comments and minor changes.

--- jQueryHideOnce.js
+++ jQueryHideOnce.js
@@ -1,8 +1,15 @@
 +function($) {
 	var $nativeShow = $.fn.show;
+	/**
+	 * Description: Hides an element with a singly bound click listener, after show.
+	 * Param: String - Selector for the element to be hidden.
+	 * Usage: $('#showMeOnce').show('#showMeOnce') - Hides `showMeOnce` on click outside it.
+	 **/
 	$.fn.show = function(hide) {
+		var $hide;
 		if(typeof hide === 'string') {
-			$(document).one('click', '*:not(' + hide + ')', $(hide).hide.bind(this));
+			$hide = $(hide);
+			$(document).one('click', '*:not(' + hide + ')', $hide.hide.bind($hide));
 		}
 		return $.fn.show.call(this);
 	};
